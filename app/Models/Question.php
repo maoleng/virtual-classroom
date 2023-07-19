64a786d8e2ef6429d8591a26a3d1850edcffa924
Bùi Hữu Lộc <feature451@gmail.com>
2023-07-19 17:03:57 +0700

    fix: remove questions column

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,7 +8,7 @@ class Question extends Base
 {
 
     protected $fillable = [
-        'content', 'text_answer', 'audio_answer', 'video_answer', 'lecture_id', 'user_id', 'created_at',
+        'content', 'lecture_id', 'user_id', 'created_at',
     ];
 
     public function getPrettyCreatedAtAttribute(): string
--- database/migrations/2023_07_01_150118_create_questions_table.php
+++ database/migrations/2023_07_01_150118_create_questions_table.php
@@ -14,12 +14,9 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('content');
-            $table->text('text_answer')->nullable();
-            $table->string('audio_answer')->nullable();
-            $table->string('video_answer')->nullable();
             $table->uuid('lecture_id');
             $table->foreign('lecture_id')->references('id')->on('lectures');
-            $table->uuid('user_id');
+            $table->uuid('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->dateTime('created_at');
         });
